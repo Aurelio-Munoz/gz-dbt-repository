562c4ca01c5fd008a96ed6203eadb7c7a6a4a654
Aurelio Munoz <aureliomunozt@gmail.com>
2025-02-12 15:06:51 +0000

    commit 2

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,12 @@
 select
    a.orders_id,
    a.date_date,
-   b.products_id,
+   b.product_id,
    sum(a.revenue) as revenue,
    sum(a.quantity) as quantity,
    sum(purchase_price) as purchase_price,
    sum(purchase_price * quantity) as purchase_cost,
    sum(revenue - (purchase_price * quantity)) as margin
 from {{ ref('stg_raw__sales') }} as a
-left join {{ ref('stg_raw__products') }} as b on a.product_id = b.products_id
-group by a.date_date, a.orders_id, b.products_id
+left join {{ ref('stg_raw__products') }} as b on a.product_id = b.product_id
+group by a.date_date, a.orders_id, b.product_id
